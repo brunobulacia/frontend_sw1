3e03be6407998171f035b1783b6f6c4e4ad0331f
S3B4S5 <sebastian75601012@outlook.com>
2025-09-01 14:49:59 -0400

    Lol

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -4,7 +4,6 @@ export default function Page() {
   return (
     <main className="p-6 flex justify-center items-center flex-col">
       <h1 className="text-xl font-semibold mb-4">Dashboard Protegido</h1>
-      <LogoutButton />
     </main>
   );
 }
